5aa14eb254bc64d0661f5c58555a86e90a34c918
ShreevathsavRao <61911268+ShreevathsavRao@users.noreply.github.com>
2023-09-29 15:02:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -186,4 +186,4 @@ def process_input():
     return jsonify(error='Input value not provided'), 400
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=8080)
+    app.run(debug=False,host='0.0.0.0',port=5000)
